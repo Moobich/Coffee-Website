32c2d9ecadbc987e828b9661b1ad970c1780763c
Moobich <zeronicpak@gmail.com>
2025-07-09 14:44:43 +0500

    Coffee-Website Data
    A single landing page web design for Coffee Types and coffee processing information.

---
+++ cofeeweb/script.js
@@ -0,0 +1,24 @@
+// function home(){
+// scrollTo(0, 0); 
+// }
+
+// function about(){
+// scrollTo(0, 960); 
+// scroll
+// }
+
+function privacyHover(){
+    window.open("https://www.freeprivacypolicy.com/blog/privacy-policy-url/");
+}
+
+function fbClick(){
+    window.open("https://www.facebook.com/");
+}
+
+function instaClick(){
+    window.open("https://www.instagram.com/");
+}
+
+function xClick(){
+    window.open("https://x.com/");
+}
